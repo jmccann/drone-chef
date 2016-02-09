496a2ddd7d3f32bd1156148c5c7f7e4ca9c64d90
Jacob McCann <jacob.mccann2@target.com>
2016-02-09 09:25:12 -0600

    Add inline documentation

--- lib/drone_chef/config.rb
+++ lib/drone_chef/config.rb
@@ -12,53 +12,97 @@ module DroneChef
       verify_reqs
     end
 
+    #
+    # Write config files such as netrc and knife keyfile
+    #
     def write_configs
       write_netrc
       write_key
     end
 
-    def boolean?(arg)
-      !!arg == arg # rubocop:disable DoubleNegation
-    end
-
+    #
+    # Determine if we are running with debugging
+    #
+    # @return [TrueClass, FalseClass]
+    #
     def debug?
       return false unless boolean?(@data['vargs']['debug']) || env_debug?
       @data['vargs']['debug'] || env_debug?
     end
 
+    #
+    # Path to the workspace
+    #
     def workspace
       @data['workspace']['path']
     end
 
+    #
+    # Server to upload to
+    #
     def server
       @data['vargs']['server']
     end
 
+    #
+    # Type of server to upload to
+    #
     def type
       set_default(__method__, 'supermarket')
     end
 
+    #
+    # User to auth to server as
+    #
     def user
       @data['vargs']['user']
     end
 
+    #
+    # The path to write our knife keyfile to
+    #
     def key_path
       '/tmp/key.pem'
     end
 
+    #
+    # Flag on wheter to use SSL verification
+    #
+    # @return [TrueClass, FalseClass]
+    #
     def ssl_verify
       set_default(__method__, true)
     end
 
+    #
+    # Knife flag for enabling/disabling SSL verify
+    #
+    # @return [String]
+    #
     def ssl_verify_mode
       ssl_verify ? ':verify_peer' : ':verify_none'
     end
 
+    #
+    # Knife config file location
+    #
     def knife_rb
       "#{Dir.home}/.chef/knife.rb"
     end
 
     private
+
+    #
+    # Determine if arg is a true/false boolean
+    #
+    # @param [Object] Object to check TrueClass/FalseClass of
+    #
+    # @return [TrueClass, FalseClass] return TrueClass if Object was TrueClass or FalseClass,
+    #   otherwise return FalseClass
+    #
+    def boolean?(arg)
+      !!arg == arg # rubocop:disable DoubleNegation
+    end
 
     #
     # Returns default value if one isn't provided
@@ -89,6 +133,9 @@ module DroneChef
       fail 'Server URL required' unless @data['vargs'].key? 'server'
     end
 
+    #
+    # Write knife keyfile
+    #
     def write_key
       puts 'INFO: Writing temp key'
       File.open(key_path, 'w') do |f|
@@ -96,14 +143,25 @@ module DroneChef
       end
     end
 
+    #
+    # Check ENV if DEBUG is set
+    #
+    # @return [TrueClass, FalseClass]
+    #
     def env_debug?
       ENV['DEBUG'] == 'true'
     end
 
+    #
+    # Datastructure of netrc info from Drone
+    #
     def netrc
       @data['workspace']['netrc']
     end
 
+    #
+    # Write .netrc file
+    #
     def write_netrc
       File.open("#{Dir.home}/.netrc", 'w') do |f|
         f.puts "machine #{netrc['machine']}"
